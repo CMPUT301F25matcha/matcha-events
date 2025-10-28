59b8abceba460ba1c296963831d36753fcde9ab6
Aditya Vaidya <avaidya1@ualberta.ca>
2025-10-28 16:29:45 -0600

    Converted project to use basic views and navigation graph

---
+++ android/app/src/main/java/com/example/lotterysystemproject/UserInfo.java
@@ -0,0 +1,44 @@
+package com.example.lotterysystemproject;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.lotterysystemproject.databinding.UserInfoBinding;
+
+public class UserInfo extends Fragment {
+
+    private UserInfoBinding binding;
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = UserInfoBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonFirst.setOnClickListener(v ->
+                NavHostFragment.findNavController(UserInfo.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment)
+        );
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
