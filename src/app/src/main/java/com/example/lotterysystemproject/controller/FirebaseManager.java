1882eb7755d63584b232784b04f8c7d6c9b9c2a8
Zifan <zifan.li912@gmail.com>
2025-10-28 11:04:02 -0600

    Updated FirebaseManager.java

--- src/app/src/main/java/com/example/lotterysystemproject/controller/FirebaseManager.java
+++ src/app/src/main/java/com/example/lotterysystemproject/controller/FirebaseManager.java
@@ -1,6 +1,60 @@
 package com.example.lotterysystemproject.controller;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
 
+/**
+ * Manages access to Firebase Firestore and Storage instances.
+ *
+ * <p>This class implements the Singleton Design pattern to ensure that only one instance of
+ * FirebaseManager exists throughout the application.<p>
+ *
+ * <p>It provides global access to the Firebase Firestore and Firebase Storage services used in
+ * the app.</p>
+ *
+ */
 public class FirebaseManager {
+
+    // Reference to the Firebase Firestore database.
+    private final FirebaseFirestore db;
+
+    // Singleton instance of FirebaseManager
+    private static FirebaseManager instance;
+
+    // Reference to Firebase Storage
+    private final FirebaseStorage storage;
+
+    /**
+     * Initializes Firestore and Storage instances.
+     */
+    private FirebaseManager() {
+        db = FirebaseFirestore.getInstance();
+        storage = FirebaseStorage.getInstance();
+    }
+
+    /**
+     * Uses double-checked locking to ensure thread-safe
+     *
+     * @return The singleton instance of FirebaseManager
+     */
+    public static FirebaseManager getInstance() {
+        if (instance == null) {
+            synchronized (FirebaseManager.class) {
+                if (instance == null) {
+                    instance = new FirebaseManager();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public FirebaseFirestore getDatabase() {
+        return db;
+    }
+
+    public FirebaseStorage getStorage() {
+        return storage;
+    }
+
 
 
 }
