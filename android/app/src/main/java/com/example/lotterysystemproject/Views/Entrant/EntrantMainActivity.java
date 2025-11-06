b75a837faa3fa4029d5919a0c805d1cd848676f7
William Li <qq1450377985@gmail.com>
2025-11-06 02:33:21 -0700

    FEAT: PARTIAL MERGE FROM ORIGIN/ADITYA: MERGE CONFLICTS NOT INCLUDED
    Squashed commit of the following:
    
    commit 510fed5b3fa8ff29f7538c3894f1baf02551e7c9
    Author: William Li <qq1450377985@gmail.com>
    Date:   Thu Nov 6 02:00:33 2025 -0700
    
        chore: .gitignore v1; Untracked deploymentTargetSelector.xml
    
    commit 83709b5537aa951c9fc41d2efe13fd72c5570eda
    Merge: ff8b2d8 3b11bcf
    Author: Aditya Vaidya <avaidya1@ualberta.ca>
    Date:   Wed Nov 5 12:52:53 2025 -0700
    
        Merge branch 'Aditya-Test' into Aditya
    
    commit 3b11bcf1360399f8c6ff7384664138661490d264
    Author: Aditya Vaidya <avaidya1@ualberta.ca>
    Date:   Wed Nov 5 12:47:01 2025 -0700
    
        fixes
    
    commit d8644547476b8325b222f09b8906363f80a493c2
    Merge: dc24662 869056f
    Author: Aditya Vaidya <avaidya1@ualberta.ca>
    Date:   Wed Nov 5 12:20:59 2025 -0700
    
        Merge branch 'Aditya-Test' of https://github.com/CMPUT301F25matcha/matcha-events into Aditya-Test
    
    commit dc24662649571bed7cdc6ce1eba402342d938c89
    Author: Aditya Vaidya <avaidya1@ualberta.ca>
    Date:   Wed Nov 5 12:19:22 2025 -0700
    
        Finished implementation of Events and EventViews
    
    commit 869056f010f3898b816d9dda9c1f8c50ed99b3e3
    Author: Aditya Vaidya <avaidya1@ualberta.ca>
    Date:   Tue Nov 4 21:15:01 2025 -0700
    
        fix firebase join in EventListHelper.java
    
    commit 6ebad01aaa97687ddb391986458f488b0fe87a71
    Author: Aditya Vaidya <avaidya1@ualberta.ca>
    Date:   Tue Nov 4 15:57:34 2025 -0700
    
        Implementing Event List
    
    commit ff8b2d812671c6309762f34ba6caa0ba4b98537f
    Merge: 447d071 48aeb4d
    Author: Aditya Vaidya <avaidya1@ualberta.ca>
    Date:   Tue Nov 4 15:22:36 2025 -0700
    
        Merge branch 'Aditya' of https://github.com/CMPUT301F25matcha/matcha-events into Aditya
    
    commit 447d0718c1a08e26edf0e95ee30ac5e71f17675f
    Author: Aditya Vaidya <avaidya1@ualberta.ca>
    Date:   Tue Nov 4 15:21:22 2025 -0700
    
        update xmls
    
    commit 48aeb4d5e23c8056be794925aa39a0649806b44d
    Author: Aditya Vaidya <avaidya1@ualberta.ca>
    Date:   Tue Nov 4 15:15:08 2025 -0700
    
        Added Event List View (user home page)

--- android/app/src/main/java/com/example/lotterysystemproject/Views/Entrant/EntrantMainActivity.java
+++ android/app/src/main/java/com/example/lotterysystemproject/Views/Entrant/EntrantMainActivity.java
@@ -3,13 +3,18 @@ package com.example.lotterysystemproject.Views.Entrant;
 import android.app.SearchManager;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.lotterysystemproject.Helpers.EventListHelper;
 import com.example.lotterysystemproject.databinding.EventViewsBinding;
+import android.view.View;
+import android.util.Log;
 
 public class EntrantMainActivity extends AppCompatActivity {
     private EventViewsBinding binding;
+    private EventListHelper eventListHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +23,8 @@ public class EntrantMainActivity extends AppCompatActivity {
         binding = EventViewsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        // Setup search view
         SearchView searchView = binding.eventSearchBar;
-
         if (searchView != null) {
             SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
             ComponentName componentName = new ComponentName(this, SearchableActivity.class);
@@ -27,5 +32,57 @@ public class EntrantMainActivity extends AppCompatActivity {
             searchView.setIconifiedByDefault(false);
         }
 
+        // Launch details on featured_events_card click
+        binding.featuredEventsCard.setOnClickListener(v -> {
+            Log.d("EntrantMainActivity", "Featured card clicked");
+            Object tag = binding.featuredEventsCard.getTag();
+            Log.d("EntrantMainActivity", "Featured card tag: " + tag);
+            String eventId = (tag instanceof String) ? (String) tag : null;
+            launchEventDetails(eventId);
+        });
+
+        // Setup events list with callback to set up listeners
+        eventListHelper = new EventListHelper(this, binding.eventsListContainer, this::setupEventCardListeners);
+        eventListHelper.loadEvents();
+    }
+
+    /**
+     * Sets up click listeners for event cards in the events list.
+     * Called after EventListHelper finishes populating the container.
+     */
+    private void setupEventCardListeners() {
+        Log.d("EntrantMainActivity", "Setting up event card listeners");
+        for (int i = 0; i < binding.eventsListContainer.getChildCount(); i++) {
+            View card = binding.eventsListContainer.getChildAt(i);
+            int finalI = i;
+            card.setOnClickListener(v -> {
+                Log.d("EntrantMainActivity", "Event card clicked at index " + finalI);
+                Object tag = v.getTag();
+                Log.d("EntrantMainActivity", "Card tag: " + tag);
+                String eventId = (tag instanceof String) ? (String) tag : null;
+                launchEventDetails(eventId);
+            });
+        }
+    }
+
+    // Utility method to start details activity
+    private void launchEventDetails(String eventId) {
+        Log.d("EntrantMainActivity", "launchEventDetails called with ID: " + eventId);
+        if (eventId == null || eventId.isEmpty()) {
+            Log.w("EntrantMainActivity", "Event ID is null or empty, cannot launch details");
+            return;
+        }
+        Intent intent = new Intent(this, EventDetailsActivity.class);
+        intent.putExtra("eventId", eventId);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Refresh events list when returning to this activity
+        if (eventListHelper != null) {
+            eventListHelper.loadEvents();
+        }
     }
 }
--- android/app/src/main/java/com/example/lotterysystemproject/Views/Entrant/UserInfoView.java
+++ android/app/src/main/java/com/example/lotterysystemproject/Views/Entrant/UserInfoView.java
@@ -7,14 +7,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.lotterysystemproject.Controllers.UserInfo;
 import com.example.lotterysystemproject.databinding.UserInfoBinding;
 
+/**
+ * View class for the user information sign-up screen.
+ * This activity allows entrants to provide their personal information
+ * including name, email, and optional phone number.
+ *
+ * Part of the MVC pattern where this class handles UI inflation and
+ * delegates user interactions to the UserInfo controller.
+ *
+ * Related User Stories:
+ * - US 01.02.01: As an entrant, I want to provide my personal information
+ * - US 01.07.01: As an entrant, I want to be identified by my device
+ *
+ * @see UserInfo
+ * @see com.example.lotterysystemproject.Models.User
+ */
 public class UserInfoView extends AppCompatActivity {
 
     private UserInfoBinding binding;
     private final UserInfo controller = new UserInfo();
 
+    /**
+     * Called when the activity is first created.
+     * Checks if the user has already completed sign-up. If so, skips this screen
+     * and navigates directly to the main activity. Otherwise, inflates the layout
+     * and sets up click listeners for the continue, skip, and admin login buttons.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *                          previously being shut down, this contains the data
+     *                          it most recently supplied. Otherwise null.
+     */
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Check if user has already signed up
+        if (controller.isUserAlreadySignedUp(this)) {
+            // User exists, skip sign-up and go to main activity
+            controller.navigateToEntrantHome(this);
+            return;
+        }
+
+        // First time user, show sign-up screen
         binding = UserInfoBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -29,6 +63,10 @@ public class UserInfoView extends AppCompatActivity {
         }
     }
 
+    /**
+     * Called before the activity is destroyed.
+     * Cleans up the view binding reference to prevent memory leaks.
+     */
     @Override
     protected void onDestroy() {
         super.onDestroy();
